01fff1e6072f86b561b442a2d4dfe8bd7dbead9d
MacroMew <kcmoshi9@gmail.com>
2023-01-17 15:09:01 -0500

    Initial Commit
    Initial Branch commit of the first physics test.

---
+++ ChitPhysicsTest1/Assets/ThrowBall.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowBall : MonoBehaviour
+{
+    Rigidbody rb;
+    public int horizontalforce = 100;
+    public int verticalforce = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("HELLO");
+            rb.useGravity = true;
+            rb.AddForce(new Vector3(0, verticalforce, horizontalforce));
+        }
+    }
+}
