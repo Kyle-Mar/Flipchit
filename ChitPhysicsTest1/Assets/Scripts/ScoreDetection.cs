060459cd8044f17dec8ae9783da9f3d8ce85588c
MacroMew <kcmoshi9@gmail.com>
2023-02-01 20:50:27 -0500

    Score Calculation and Event
    - Upon the rigidbody of the chit sleeping. The scoring system will check if the chit had flipped over in that time and was hit.
    - The score will be calculated from there and an Event is Invoked.
    - This should be used in a Game Manager script to update the score.

---
+++ ChitPhysicsTest1/Assets/Scripts/ScoreDetection.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ScoreDetection : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    bool chitHasBeenHit;
+    bool chitHasFlipped;
+    Vector3 initUpDir;
+    Rigidbody rb;
+
+    // this is called after the points are calculated.
+    // This will be used in order to send new information to the 
+    // Game manager script.
+    public delegate void OnScore(float score);
+    public static event OnScore OnPointsCalculated;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(chitHasBeenHit == false)
+        {
+            return;
+        }
+        float angleBetween = Vector3.Angle(initUpDir, transform.forward);
+
+        // this is basically a guess
+        // 100 and not 90 to account for the degree of stability of the chit.
+        
+        if (angleBetween > 100)
+        {
+            chitHasFlipped = true;
+        }
+        if (rb.IsSleeping()) {
+            CalculatePoints(chitHasBeenHit, chitHasFlipped);
+            chitHasBeenHit = false;
+        }
+    }
+
+    private void CalculatePoints(bool hit, bool flipped)
+    {
+        int points = 0;
+        if(hit)
+        {
+            points++;
+        }
+        if(flipped)
+        {
+            points += 2;
+        }
+        OnPointsCalculated?.Invoke(points);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            chitHasBeenHit = true;
+            chitHasFlipped = false;
+            initUpDir = transform.forward;
+        }
+    }
+}
