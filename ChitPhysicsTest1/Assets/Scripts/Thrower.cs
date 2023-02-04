5a844ec6149c9cbe2d9f75acdaede4c660b3e86c
MacroMew <kcmoshi9@gmail.com>
2023-02-03 19:13:27 -0500

    Fix Conflicts

--- ChitPhysicsTest1/Assets/Scripts/Thrower.cs
+++ ChitPhysicsTest1/Assets/Scripts/Thrower.cs
@@ -5,154 +5,138 @@ using Unity.Netcode;
 
 namespace FlipChit
 {
-public class Thrower : NetworkBehaviour
-{
-    // Start is called before the first frame update
-
-    public GameObject target;
-    public GameObject ball;
-    public GameObject StartPos;
-    
-    Ball ballScript;
-    float maxSpinSpeed = 25;
-    float minSpinSpeed = 2;
-    public float maxThrowVar = 15;
-
-    Vector2 beginTouchPosition, endTouchPosition;
-    
-    
-    private Touch touch;
-    // Start is called before the first frame update
-    void Start()
+    public class Thrower : NetworkBehaviour
     {
-        target = GameObject.Find("Target");
-        ball = GameObject.Find("Ball");
-        ballScript = ball.GetComponent<Ball>();
-    }
+        // Start is called before the first frame update
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (!IsOwner)
+        public GameObject target;
+        public GameObject ball;
+        public GameObject StartPos;
+
+        Ball ballScript;
+        float maxSpinSpeed = 25;
+        float minSpinSpeed = 2;
+        public float maxThrowVar = 15;
+
+        Vector2 beginTouchPosition, endTouchPosition;
+
+
+        private Touch touch;
+        // Start is called before the first frame update
+        void Start()
         {
-            return;
+            target = GameObject.Find("Target");
+            ball = GameObject.Find("Ball");
+            ballScript = ball.GetComponent<Ball>();
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && !ballScript.InPlay)
+        // Update is called once per frame
+        void Update()
         {
-            if (IsClient) {
-                ThrowServerRpc();        
+            if (!IsOwner)
+            {
+                return;
             }
-            else
+
+            if (Input.touchCount <= 0)
+            {
+                return;
+            }
+
+            touch = Input.GetTouch(0);
+            switch (touch.phase)
+            {
+
+                case TouchPhase.Began:
+                    beginTouchPosition = touch.position;
+                    break;
+
+                case TouchPhase.Ended:
+                    endTouchPosition = touch.position;
+
+                    float varianceZ = CalculateVariance(touch);
+                    float rotSpeed = CalculateRotationSpeed(touch);
+
+                    if (beginTouchPosition.x <= endTouchPosition.x && !ballScript.InPlay)
+                    {
+                        if (IsClient)
+                        {
+                            ThrowServerRpc(varianceZ, rotSpeed, Ball.SpinDirection.CLOCKWISE);
+                        }
+                        else
+                        {
+                            Throw(varianceZ, rotSpeed, Ball.SpinDirection.CLOCKWISE);
+                        }
+                    }
+
+                    else if (beginTouchPosition.x > endTouchPosition.x && !ballScript.InPlay)
+                    {
+                        if (IsClient)
+                        {
+                            ThrowServerRpc(varianceZ, rotSpeed, Ball.SpinDirection.COUNTERCLOCKWISE);
+                        }
+                        else
+                        {
+                            Throw(varianceZ, rotSpeed, Ball.SpinDirection.COUNTERCLOCKWISE);
+                        }
+                    }
+
+                    if (beginTouchPosition == endTouchPosition)
+                    {
+                        ballScript.ResetBall(StartPos.transform.position);
+                    }
+                    break;
+
+                default:
+                    break;
+
+            }
+
+            /*
+            if (Input.GetKeyDown(KeyCode.Space) && !ballScript.InPlay)
             {
                 ballScript.ThrowAt(target.transform.position);
                 ballScript.Spin(Ball.SpinDirection.CLOCKWISE, 0, 0);
             }
-            
+
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                ballScript.ResetBall(StartPos.transform.position);
+            }*/
+
         }
-        if (Input.GetKeyDown(KeyCode.R))
+        // This is a lot of data to send... How fix?
+        [ServerRpc]
+        void ThrowServerRpc(float varZ, float rotSpeed, Ball.SpinDirection direction)
         {
-            ballScript.ResetBall(StartPos.transform.position);
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(!IsOwner){
-            return;
+            Throw(varZ, rotSpeed, direction);
         }
 
-        if(Input.touchCount <= 0)
+        void Throw(float varZ, float rotSpeed, Ball.SpinDirection direction)
         {
-            return;
+            ballScript.Spin(Ball.SpinDirection.CLOCKWISE, rotSpeed, 5);
+            ballScript.ThrowAt(target.transform.position, 0, varZ);
         }
 
-        touch = Input.GetTouch(0);
-        switch (touch.phase) {
 
-            case TouchPhase.Began:
-                beginTouchPosition = touch.position;
-                break;
-
-            case TouchPhase.Ended:
-                endTouchPosition = touch.position;
-                
-                float varianceZ = CalculateVariance(touch);
-                float rotSpeed = CalculateRotationSpeed(touch);
-
-                if (beginTouchPosition.x <= endTouchPosition.x && !ballScript.InPlay) {
-                    if(IsClient){
-                        ThrowServerRpc(varZ, rotSpeed, Ball.SpinDirection.CLOCKWISE);
-                    }
-                    else{
-                        Throw(varZ, rotSpeed, Ball.SpinDirection.CLOCKWISE); 
-                    }
-                }
-
-                else if(beginTouchPosition.x > endTouchPosition.x && !ballScript.InPlay)
-                {
-                    if(IsClient){
-                        ThrowServerRpc(varZ, rotSpeed, Ball.SpinDirection.COUNTERCLOCKWISE);
-                    }
-                    else{
-                        Throw(varZ, rotSpeed, Ball.SpinDirection.COUNTERCLOCKWISE);    
-                    }
-                }
-
-                if(beginTouchPosition == endTouchPosition)
-                {
-                    ballScript.ResetBall(StartPos.transform.position);
-                }
-                break;
-
-            default:
-                break;
-            
-        }
-
-        /*
-        if (Input.GetKeyDown(KeyCode.Space) && !ballScript.InPlay)
+        float CalculateRotationSpeed(Touch touch)
         {
-            ballScript.ThrowAt(target.transform.position);
-            ballScript.Spin(Ball.SpinDirection.CLOCKWISE, 0, 0);
+            float speed = Mathf.Min(Mathf.Abs(touch.deltaPosition.x / (touch.deltaTime * 5f)), maxSpinSpeed);
+            return speed < minSpinSpeed ? minSpinSpeed : speed;
         }
 
-        if (Input.GetKeyDown(KeyCode.R))
+        float CalculateVariance(Touch touch)
         {
-            ballScript.ResetBall(StartPos.transform.position);
-        }*/
-
-    }
-    // This is a lot of data to send... How fix?
-    [ServerRpc]
-    void ThrowServerRpc(float varZ, float rotSpeed, Ball.SpinDirection direction,){
-        Throw(varZ, rotSpeed, Ball.SpinDirection direction)
-    }
-
-    void Throw(float varZ, float rotSpeed, Ball.SpinDirection direction,){
-        ballScript.Spin(Ball.SpinDirection.CLOCKWISE, rotSpeed, 5);
-        ballScript.ThrowAt(target.transform.position, 0, varZ);
-    }
-    
-    
-    float CalculateRotationSpeed(Touch touch)
-    {
-        float speed = Mathf.Min(Mathf.Abs(touch.deltaPosition.x / (touch.deltaTime * 5f)), maxSpinSpeed);
-        return speed < minSpinSpeed ? minSpinSpeed : speed;
-    }
-
-    float CalculateVariance(Touch touch)
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
-        RaycastHit hit;
-        Physics.Raycast(ray, out hit, Mathf.Infinity);
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit hit;
+            Physics.Raycast(ray, out hit, Mathf.Infinity);
 
 
-        float distance = Vector3.Distance(hit.point, target.transform.position);
-        float variance = distance * Mathf.Sign(hit.point.z - target.transform.position.z);
+            float distance = Vector3.Distance(hit.point, target.transform.position);
+            float variance = distance * Mathf.Sign(hit.point.z - target.transform.position.z);
 
-        Debug.Log(distance + " " + variance);
-        return variance;
+            Debug.Log(distance + " " + variance);
+            return variance;
+        }
     }
 }
